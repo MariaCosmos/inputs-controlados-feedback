bd67a410e8f2ddc66f0e34da6e42168b5a3f1add
MariaCosmos <cosmos.maria2003@gmail.com>
2022-10-19 08:31:24 -0300

    Exercicios concluidos - Inputs controlados

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -23,13 +23,25 @@ const onChangeEmail = (event) => {
 
 const sendData = () => {
   //aqui deve vir uma verificação para mudar de formulario apenas se todos os requisitos tiverem sido cumpridos
-  setFormFlow(2)
+  if(name === ("") || age === ("") || email === ("")){
+    alert("Todos os campos são obrigatórios, preencha-os")
+  }else if (name.length < 10 & name.length > 30){
+    alert("O nome deve ter de 10 a 30 caracteres")
+  }else if (!email.includes("@") & !email.includes(".com")){
+    alert("formato do email incorreto")
+  }else if (age < 18){
+    alert("só maiores de 18 anos podem se inscrever na Labenu")
+  }else {
+    console.log({name, age, email})
+    setFormFlow(2)
+  }
+  
 }
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       {formFlow === 1 ? <NameForm
-      // insira aqui suas props
+      name={name} age={age} email={email} onChangeName={onChangeName} onChangeAge={onChangeAge} onChangeEmail={onChangeEmail} sendData={sendData}
       /> : <ConfirmationForm />}
     </MainContainer>
   )
